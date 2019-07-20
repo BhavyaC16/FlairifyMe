d532653f36eaf135ef463c3a06fab7f98bd9b815
Bhavya <bhavya18333@iiitd.ac.in>
2019-07-20 20:49:08 +0530

    updated plot

--- Data/Scripts/splitVis.py
+++ Data/Scripts/splitVis.py
@@ -5,4 +5,5 @@ df = pd.read_csv("/home/bhavya16/Desktop/FlairifyMe/Data/CSV/preprocessedData.cs
 flair = ['AMA','Business Finance Policy','Food','Sports','Politics','Science Technology','Policy Economy','Photography','Scheduled','Reddiquette','Non-Political','AskIndia','Entertainment']
 plt.figure()
 df.flair.value_counts().plot(kind='bar');
+plt.savefig('DataSplit.png', bbox_inches = "tight")
 plt.show()
