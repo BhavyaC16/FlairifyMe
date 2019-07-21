adb142c77660115464bde57adce2a6ac3de35695
Bhavya <bhavya18333@iiitd.ac.in>
2019-07-21 16:37:42 +0530

    saved logistic regression model

---
+++ Training/flair_predictor.py
@@ -0,0 +1,3 @@
+import joblib
+
+model = joblib.load("./Models/finalized_model.sav")
--- Training/text_classifier.py
+++ Training/text_classifier.py
@@ -1,5 +1,4 @@
 from pymongo import MongoClient
-import logging
 import pandas as pd
 import numpy as np
 from numpy import random
@@ -16,6 +15,7 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 import re
+import pickle
 
 flairs = ['AMA','Business Finance Policy','Food','Sports','Politics','Science Technology','Policy Economy','Photography','Scheduled','Reddiquette','Non-Political','AskIndia','Entertainment']
 
@@ -36,10 +36,11 @@ def linear_SVM(x_train,y_train,x_test,y_test):
 def logicalRegression(x_train,y_train,x_test,y_test):
 	logreg = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', LogisticRegression(n_jobs=1, C=1e5))])
 	logreg.fit(x_train,y_train)
+	filename = "./Models/finalized_model.sav"
+	pickle.dump(logreg,open(filename,'wb'))
 	y_pred = logreg.predict(x_test)
 	print("Logical Regression: "+str(accuracy_score(y_pred,y_test)))
 	print(classification_report(y_test, y_pred,target_names=flairs))	
-
 
 
 client = MongoClient('mongodb+srv://BhavyaC16:nancydrew@flairifyme-jji1m.mongodb.net/test?retryWrites=true&w=majority')
@@ -51,10 +52,15 @@ df = df.fillna("")
 df_withStemming = pd.DataFrame(list(preprocessedData_withStemming.find()))
 df_withStemming = df_withStemming.fillna("")
 
-x = df.title_words
-y = df.flair
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.25, random_state=10)
+combined = df['title_words']+df['body_words']+df['comment_words']
+df = df.assign(combined=combined)
+combined_withStemming = df_withStemming['title_words']+df_withStemming['body_words']+df_withStemming['comment_words']
+df_withStemming = df_withStemming.assign(combined=combined_withStemming)
 
-naiveBayes(x_train,y_train,x_test,y_test)
-linear_SVM(x_train,y_train,x_test,y_test)
+x = df.combined
+y = df.flair
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.20, random_state=10)
+
+#naiveBayes(x_train,y_train,x_test,y_test)
+#linear_SVM(x_train,y_train,x_test,y_test)
 logicalRegression(x_train,y_train,x_test,y_test)
